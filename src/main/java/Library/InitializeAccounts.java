cecb2047af51b6103e788c98db3b47c6a9e9d42a
EuropeanB <Qq1042187714@163.com>
2025-09-27 20:17:51 -0400

    REFAC - Modified the R-TEST-06: Add codes to test the usernames and passwords
    RESP_01_test_01
    RESP_01_test_02
    RESP_01_test_03
    RESP_01_test_04
    RESP_01_test_05
    RESP_02_test_01

--- src/main/java/Library/InitializeAccounts.java
+++ src/main/java/Library/InitializeAccounts.java
@@ -3,11 +3,7 @@ package Library;
 public class InitializeAccounts {
 
     public Accounts initializeAccounts(){
-        Accounts accounts = new Accounts();
-        accounts.add(new Borrower("borrower01", "123"));
-        accounts.add(new Borrower("borrower02", "456"));
-        accounts.add(new Borrower("borrower03", "789"));
-        return accounts;
+        return new Accounts();
     }
 
 }
--- src/test/java/Library/RESP_02_Test.java
+++ src/test/java/Library/RESP_02_Test.java
@@ -11,7 +11,21 @@ public class RESP_02_Test {
     void RESP_02_test_01(){
         InitializeAccounts initializeaccounts = new InitializeAccounts();
         Accounts accounts = initializeaccounts.initializeAccounts();
+
         assertEquals(3, accounts.size());
+
+        String[][] expected = {
+                {"borrower01", "123"},
+                {"borrower02",   "456"},
+                {"borrower03", "789"}
+        };
+
+        for (int i = 0; i < expected.length; i++) {
+            Borrower b = accounts.all().get(i);
+            assertEquals(expected[i][0], b.getUsername());
+            assertEquals(expected[i][1], b.getPassword());
+        }
     }
+
 
 }
