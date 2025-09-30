04bb6371d2991327b0ebf921dea6263a252f1433
EuropeanB <Qq1042187714@163.com>
2025-09-29 23:00:08 -0400

    R-TEST-20 - Check the system checking during borrowing
    RESP_01_test_01
    RESP_01_test_02
    RESP_01_test_03
    RESP_01_test_04
    RESP_01_test_05
    RESP_02_test_01
    RESP_02_test_02
    RESP_03_test_01
    RESP_03_test_02
    RESP_04_test_01
    RESP_05_test_01
    RESP_06_test_01
    RESP_06_test_02
    RESP_07_test_01
    RESP_08_test_01
    RESP_08_test_02
    RESP_09_test_01
    RESP_10_test_01
    RESP_10-test_02

---
+++ src/test/java/Library/RESP_11_Test.java
@@ -0,0 +1,40 @@
+package Library;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+// Check the system checking during borrowing
+// UC-02: 5-7
+
+public class RESP_11_Test {
+    private Catalogue catalogue;
+    private Accounts accounts;
+    private holdList holds;
+    private Book book01;
+    private Borrower borrower01;
+    private Borrower borrower02;
+
+    @BeforeEach
+    void setUp() {
+        catalogue = new InitializeLibrary().initializeLibrary();
+        accounts = new InitializeAccounts().initializeAccounts();
+        holds = new holdList();
+
+        book01 = catalogue.getBook(0);
+        borrower01 = accounts.all().get(0);
+        borrower02 = accounts.all().get(1);
+
+    }
+
+    @Test
+    @DisplayName("Check the system checking during borrowing")
+    public void RESP_11_test_01(){
+        book01.setStatus(Book.Status.Checked_out);
+        assertEquals(Book.Status.Checked_out, book01.getStatus());
+
+    }
+
+}
