d72db0e390b39ec2489352ecb2813fe89dd7085e
EuropeanB <Qq1042187714@163.com>
2025-10-02 22:06:31 -0400

    R-TEST-27 - Check System updating on account and book status
    RESP_01_test_01
    RESP_01_test_02
    RESP_01_test_03
    RESP_01_test_04
    RESP_01_test_05
    RESP_02_test_01
    RESP_02_test_02
    RESP_03_test_01
    RESP_03_test_02
    RESP_04_test_01
    RESP_05_test_01
    RESP_06_test_01
    RESP_06_test_02
    RESP_07_test_01
    RESP_08_test_01
    RESP_08_test_02
    RESP_09_test_01
    RESP_10_test_01
    RESP_10-test_02
    RESP_11_test_01
    RESP_11_test_02
    RESP_11_test_03
    RESP_12_test_01
    RESP_13_test_01
    RESP_13_test_02
    RESP_14_test_01
    RESP_15_test_01

---
+++ src/test/java/Library/RESP_15_Test.java
@@ -0,0 +1,58 @@
+package Library;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+
+public class RESP_15_Test {
+    private InputStream originalIn;
+    private PrintStream originalOut;
+    private ByteArrayOutputStream capturedOut;
+
+    @BeforeEach
+    void setUp() {
+        originalIn = System.in;
+        originalOut = System.out;
+
+        capturedOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(capturedOut));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setIn(originalIn);
+        System.setOut(originalOut);
+    }
+
+    @Test
+    @DisplayName("Check System updating on account and book status")
+    public void RESP_15_test_01() {
+        String input = "borrower02\n456\n1\n20\ny\ny\n1\n";
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+
+        try {
+            new Control().launch();
+        } catch (Exception ignored) {
+
+        } finally{
+            System.setIn(originalIn);
+            System.setOut(originalOut);
+        }
+
+        String output = capturedOut.toString();
+        System.out.println("------------ Output -------------");
+        System.out.println(output);
+        System.out.println("---------------------------------");
+
+        assertTrue(output.contains("You already borrowed 1/3 books!"));
+    }
+}
